7b3f6b37643ad94c410c42b74c5d045e5673f6b8
saddiqs1 <ssaddique96@hotmail.com>
2023-09-30 22:09:29 +0100

    refactor components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
-import { HomePage } from '@components/HomePage'
+import { CrosshairConverter } from '@components/CrosshairConverter'
+import { Center, Stack, Title } from '@mantine/core'
 
 export default function Home() {
-	return <HomePage />
+	return (
+		<Center h={'100vh'}>
+			<Stack spacing={'xl'}>
+				<Title order={1} ta={'center'}>
+					CS2 Crosshair Converter
+				</Title>
+				<CrosshairConverter />
+			</Stack>
+		</Center>
+	)
 }
